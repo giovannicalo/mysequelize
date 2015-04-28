6bafe33919f512c0852ae04276a2ee7b520c8a6c
Giovanni Calò <giovanni.calo@live.it>
2015-04-28 21:12:15 +0200

    Added field parser module

---
+++ parser/field.js
@@ -0,0 +1,65 @@
+"use strict";
+
+var config = require("../config");
+var constant = require("./constant");
+var log = require("../log");
+var string_case = require("../string_case");
+
+module.exports = function(field, is_last) {
+	if (field) {
+		var name = string_case.convert(field.Field, config.naming.field);
+		var language = constant.language[config.language].content;
+		var content = [];
+		content.push({
+			indentation: 2,
+			text: language.field(name)
+		});
+		if (field.Null == "NO") {
+			content.push({
+				indentation: 3,
+				text: language.property("allowNull", "false")
+			});
+		}
+		if (field.Extra == "auto_increment") {
+			content.push({
+				indentation: 3,
+				text: language.property("autoIncrement", "true")
+			});
+		}
+		if (field.Default !== null) {
+			content.push({
+				indentation: 3,
+				text: language.property("defaultValue", ((field.Default == "CURRENT_TIMESTAMP") ? "type.NOW" : field.Default))
+			});
+		}
+		if (field.Field != name) {
+			content.push({
+				indentation: 3,
+				text: language.property("field", "\"" + field.Field + "\"")
+			});
+		}
+		if (field.Key == "PRI") {
+			content.push({
+				indentation: 3,
+				text: language.property("primaryKey", "true")
+			});
+		}
+		content.push({
+			indentation: 3,
+			text: language.property("type", "type." + require("./type")(field.Type), (field.Key != "UNI"))
+		});
+		if (field.Key == "UNI") {
+			content.push({
+				indentation: 3,
+				text: language.property("unique", ((field.Key == "UNI") ? "true" : "false"), true)
+			});
+		}
+		content.push({
+			indentation: 2,
+			text: language.block_end(is_last)
+		});
+		return content;
+	} else {
+		log.error("Error: Attempting to parse a field without enough data.");
+	}
+};
