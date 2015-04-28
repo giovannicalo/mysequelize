3431097c8c688a7527d3888cb6b383e42cc885b6
Giovanni Calò <giovanni.calo@live.it>
2015-04-28 10:50:29 +0200

    Added configuration module and default values

---
+++ config.js
@@ -0,0 +1,24 @@
+"use strict";
+
+module.exports = {
+
+	end_of_line: "\n",
+
+	indentation: "\t",
+
+	language: "javascript",
+
+	naming: {
+		file: "snake",
+		field: "snake",
+		model: "snake"
+	},
+
+	server: {
+		host: "localhost",
+		password: "",
+		port: 3306,
+		user: "root"
+	}
+
+};
