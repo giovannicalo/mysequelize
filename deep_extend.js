3af888f8f5ce2dd6f2d3f5db0cdbb5062cc5143b
Giovanni Calò <giovanni.calo@live.it>
2015-04-29 00:13:56 +0200

    Added deep extend module

---
+++ deep_extend.js
@@ -0,0 +1,22 @@
+"use strict";
+
+module.exports = function(destination) {
+	var i = 0;
+	var key = 0;
+	var source = null;
+	for (i = 0; i < arguments.length; i++) {
+		if (i !== 0) {
+			source = arguments[i];
+			for (key in source) {
+				if (source.hasOwnProperty(key)) {
+					if ((destination[key]) && (source[key].constructor === Object) && (destination[key].constructor === Object)) {
+						destination[key] = this(destination[key], source[key]);
+					} else {
+						destination[key] = source[key];
+					}
+				}
+			}
+		}
+	}
+	return destination;
+};
