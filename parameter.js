250afe0332377073f551d6d6f0a783ebd3b71080
Giovanni Calò <giovanni.calo@live.it>
2015-04-29 00:15:09 +0200

    Added parameters module

---
+++ parameter.js
@@ -0,0 +1,49 @@
+"use strict";
+
+module.exports = {
+
+	alias: {
+		d: "database",
+		db: "database",
+		dir: "directory",
+		e: "end_of_line",
+		eol: "end_of_line",
+		f: "directory",
+		h: "server.host",
+		host: "server.host",
+		i: "intentation",
+		indent: "indentation",
+		l: "language",
+		lang: "language",
+		p: "server.port",
+		password: "server.password",
+		port: "server.port",
+		s: "silent",
+		t: "table",
+		u: "server.user",
+		user: "server.user",
+		x: "server.password"
+	},
+
+	boolean: [
+		"silent",
+		"strict"
+	],
+
+	string: [
+		"database",
+		"directory",
+		"end_of_line",
+		"indentation",
+		"language",
+		"naming.file",
+		"naming.field",
+		"naming.model",
+		"server.host",
+		"server.password",
+		"server.port",
+		"server.user",
+		"table"
+	]
+
+};
