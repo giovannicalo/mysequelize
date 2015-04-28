8d7fe2c52bfde6894a0409bfac96482da125c480
Giovanni Calò <giovanni.calo@live.it>
2015-04-28 21:03:35 +0200

    Added main module

---
+++ main.js
@@ -0,0 +1,39 @@
+"use strict";
+
+var chalk = require("chalk");
+var minimist = require("minimist");
+var sequelize = require("sequelize");
+
+var config = require("./config");
+var log = require("./log");
+
+var parameter_list = minimist(process.argv.slice(2), {
+	alias: {
+		d: "database",
+		t: "table"
+	},
+	string: [
+		"database",
+		"table"
+	]
+});
+
+module.exports = (function(database, table) {
+	log.header();
+	if (database) {
+		database = new sequelize(database, config.server.user, config.server.password, {
+			dialiect: "mysql",
+			logging: false,
+			host: config.server.host,
+			port: config.server.port
+		});
+		require("./directory")().then(function() {
+			log.success("Target directory " + chalk.blue(config.directory) + " prepared successfully.");
+			require("./parser/database")(database, table);
+		}).catch(function(error) {
+			log.error("Error: Failed to prepare the target directory " + chalk.blue(config.directory) + ".", error);
+		});
+	} else {
+		log.error("Error: A target database is required. You can pick one with the -d or --database parameters.");
+	}
+})(parameter_list.database, parameter_list.table);
