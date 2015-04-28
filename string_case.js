8285eae10ae1613e2c4c8c0403f20742fa8795d4
Giovanni Calò <giovanni.calo@live.it>
2015-04-28 10:49:01 +0200

    Added case conversion module

---
+++ string_case.js
@@ -0,0 +1,59 @@
+"use strict";
+
+var inflection = require("inflection");
+
+module.exports = {
+
+	string: null,
+	convert: function(string, destination) {
+		if (this["to_" + destination]) {
+			return this.from(string)["to_" + destination]();
+		} else {
+			this.error(destination);
+		}
+	},
+
+	error: function(error) {
+		throw new Error("Case \"" + error + "\" is not supported.");
+	},
+
+	from: function(string) {
+		var self = this;
+		this.string = inflection.underscore(string.replace(/-/g, "_").replace(/[A-Z]{2,}/g, function(match) {
+			return self.upper_case_first(match.toLowerCase());
+		})).replace(/_{2,}/g, "_");
+		return this;
+	},
+
+	to_camel: function() {
+		return inflection.camelize(this.string, true);
+	},
+
+	to_pascal: function() {
+		return inflection.camelize(this.string);
+	},
+
+	to_screaming_snake: function() {
+		return this.string.toUpperCase();
+	},
+
+	to_snake: function() {
+		return this.string;
+	},
+
+	to_spinal: function() {
+		return inflection.dasherize(this.string);
+	},
+
+	to_train: function() {
+		var self = this;
+		return self.upper_case_first(inflection.dasherize(this.string).replace(/-[a-z]/g, function(match) {
+			return "-" + self.upper_case_first(match.slice(1));
+		}));
+	},
+
+	upper_case_first: function(string) {
+		return string.charAt(0).toUpperCase() + string.slice(1);
+	}
+
+};
